c673d264222bca3790229f2767d4282a0b43c1bb
Simran <rajputsimran.0079@gmail.com>
2025-07-08 15:07:18 +0530

    largest number and smallest number

---
+++ ARRAYS/smallest/largestNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+     cout<<"minimum value: "<<arr[0] <<" mamximum value: "<<arr[n-1]<<endl;
+        return 0;
+}
