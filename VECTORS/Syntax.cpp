2ee0d2cfec692505049a72068cdfdbeda47c212e
Simran <rajputsimran.0079@gmail.com>
2025-07-11 18:21:32 +0530

    vectors

---
+++ VECTORS/Syntax.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <char> vectoria={'a','b','c','d','e'};
+    
+    for(char val:vectoria){
+        cout<<val<<endl;
+    }
+    return 0;
+
+}
---
+++ VECTORS/functions.cpp
@@ -0,0 +1,18 @@
+// VECTORE FUNCTIONS
+/*
+SIZE
+PUSH_BACK
+POP_BACK
+FRONT
+BACK
+AT
+RESIZE
+CLEAR
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<char> vect={'a','b','c','d','e'};
+    cout<<vect.size()<<endl;
+}
