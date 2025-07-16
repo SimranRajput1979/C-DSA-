4b4020867a280cf66063fe839591a4107ab679d0
Simran <rajputsimran.0079@gmail.com>
2025-07-17 01:39:17 +0530

    syntax of vectors

--- VECTORS/Syntax.cpp
+++ VECTORS/Syntax.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int main(){
     vector <char> vectoria={'a','b','c','d','e'};
+    cout<<"size of the vector: "<<vectoria.size()
+    <<endl;
     
     for(char val:vectoria){
         cout<<val<<endl;
