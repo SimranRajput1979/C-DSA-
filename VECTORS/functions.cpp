520b252c7e20d966df9c444e7949c3d12f968503
Simran <rajputsimran.0079@gmail.com>
2025-07-17 01:39:01 +0530

    functions

--- VECTORS/functions.cpp
+++ VECTORS/functions.cpp
@@ -1,11 +1,11 @@
 // VECTORE FUNCTIONS
 /*
-SIZE
-PUSH_BACK
-POP_BACK
-FRONT
-BACK
-AT
+SIZE    //tell the size of the array
+PUSH_BACK    //add the element in the last 
+POP_BACK    // delete the last element in the list
+FRONT       //try to print the element in the first
+BACK        //give the last value
+AT          //treat it like a index vect.at(0)in brackets these are index..
 RESIZE
 CLEAR
 */
@@ -14,5 +14,13 @@ CLEAR
 using namespace std;
 int main(){
     vector<char> vect={'a','b','c','d','e'};
+    vect.push_back('p');
+    vect.pop_back();
+    cout<<vect.front()<<endl;
+    cout<<vect.back()<<endl;
+    for(char val:vect){
+        cout<<val<<endl;
+    }
     cout<<vect.size()<<endl;
+    cout<<vect.at(0)<<endl;
 }
