62ea83891b61ab1162048a83d50da7d38c70a110
Simran <rajputsimran.0079@gmail.com>
2025-07-23 01:10:53 +0530

    pointer to pointer

--- pointers/pointerTOpointer.cpp
+++ pointers/pointerTOpointer.cpp
@@ -4,7 +4,7 @@ int main(){
     int a=10;
     int *ptr=&a;
     int **ptr2=&ptr;
-    cout<<&ptr<<endl;   //both these cout will give the same output pointer in the pointer;
+    cout<<*(ptr)<<endl;   //both these cout will give the same output pointer in the pointer;
     cout<<ptr2<<endl;
     return 0;
 }
