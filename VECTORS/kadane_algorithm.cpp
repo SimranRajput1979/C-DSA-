ffced40b3b78f50e81bd8b0a2ae4252bb85f360a
Simran <rajputsimran.0079@gmail.com>
2025-07-30 01:12:54 +0530

    .

--- VECTORS/kadane_algorithm.cpp
+++ VECTORS/kadane_algorithm.cpp
@@ -20,7 +20,7 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector <int> vec={1,2,3,4,5};
+    vector <int> vec={3,-4,5,4,-1,7,-8};
     int currentsum=0;
     int maxsum=0;
     for(int val:vec){
