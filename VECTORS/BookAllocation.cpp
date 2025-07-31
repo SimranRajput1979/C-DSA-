a3cb4f410eb1c5bb6417039dccb95184d6ba1e62
Simran <rajputsimran.0079@gmail.com>
2025-07-31 13:45:29 +0530

    changes in book allocation

--- VECTORS/BookAllocation.cpp
+++ VECTORS/BookAllocation.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 bool isValid(vector<int> &arr,int n,int m,int maxallocatebooks){
     int students=1,pages=0;
@@ -22,11 +23,19 @@ int allocationOfBook(vector<int> &arr,int n,int m){
         return -1;
     }
      int sum=0;
+    //  int maxVal = arr[0];  // manually find max
+    //  for(int i = 0; i < n; i++) {
+    //      if(arr[i] > maxVal) {
+    //          maxVal = arr[i];
+    //      }
+    //  }
     for(int i=0;i<n;i++){
         sum+=arr[i];
     }
     int ans=-1;
-    int start=0,end=sum;
+    // int start=maxVal;     //if we do not want to start from 0 we can use this method by assigning the max value in the array
+    int start=0;
+    int end=sum;
     while(start<=end){
         int mid=start+(end-start)/2;
         if(isValid(arr,n,m,mid)){
@@ -37,10 +46,11 @@ int allocationOfBook(vector<int> &arr,int n,int m){
             start=mid+1;
         }
     }
+    return ans;
 }
 int main(){
     vector<int> arr={2,1,3,4};
     int n=4,m=2;
     cout<<allocationOfBook(arr,n,m)<<endl;
     return 0;
-    }
+    } 
